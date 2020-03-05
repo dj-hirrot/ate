778bf807d910ffa8028605b60b6bdeb523283927
dj-hirrot <hirotoshibutani@gmail.com>
2020-03-05 13:43:41 +0900

    updated: 国際化

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ Bundler.require(*Rails.groups)
 module Ate
   class Application < Rails::Application
     config.load_defaults 6.0
+    config.i18n.default_locale = :ja
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
