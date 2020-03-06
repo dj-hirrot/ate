306aefc831cb90d7c1c52255c5c26bb6a7fdbdf2
dj-hirrot <hirotoshibutani@gmail.com>
2020-03-06 16:40:45 +0900

    update: Cateogryモデルの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories, only: [:index]
+  end
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
