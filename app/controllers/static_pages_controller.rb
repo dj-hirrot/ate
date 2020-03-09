aa0e59cea908091d6ad0e92ae97145b4495e3398
dj-hirrot <hirotoshibutani@gmail.com>
2020-03-09 19:33:26 +0900

    updated: categories;

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate_user!, only: []
   def top
+    return if current_user.nil?
+    @categories = Category.all
   end
 end
